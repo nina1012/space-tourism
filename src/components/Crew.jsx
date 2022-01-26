af474497baaeaa9b3ddbdf5271d35bb935637ec5
Nina <labferment10@gmail.com>
2022-01-26 21:26:45 +0100

    adding swiping funcitionality

--- src/components/Crew.jsx
+++ src/components/Crew.jsx
@@ -11,6 +11,7 @@ import 'swiper/css/pagination';
 import DATA from '../../data';
 import Member from './Member';
 import { useState } from 'react';
+import '../index.css';
 
 const Crew = () => {
 	const [activeMember, setActiceMember] = useState(0);
@@ -24,10 +25,11 @@ const Crew = () => {
 			{/* </div> */}
 
 			<Swiper
-				spaceBetween={30}
-				pagination={{
-					clickable: true,
-				}}
+				modules={[Pagination]}
+				pagination={{ clickable: true }}
+				spaceBetween={100}
+				slidesPerView={1}
+				loop={true}
 				className='mySwiper'
 			>
 				{DATA.crew.map((crew, i) => {
@@ -37,17 +39,6 @@ const Crew = () => {
 						</SwiperSlide>
 					);
 				})}
-				<div className='swiper-indicators flex justify-between w-24 mx-auto '>
-					{[0, 1, 2, 3].map((indicator) => (
-						<div
-							key={indicator}
-							onClick={(e) => setActiceMember(indicator)}
-							className={`w-4 h-4 rounded-full transition-all bg-white hover:cursor-pointer	 ${
-								activeMember === indicator ? ' opacity-100' : ' opacity-10'
-							}`}
-						></div>
-					))}
-				</div>
 			</Swiper>
 		</div>
 	);
