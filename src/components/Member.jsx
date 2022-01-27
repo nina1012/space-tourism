2816c4324ec84b808eee06f1301bd60f563054ee
Nina <labferment10@gmail.com>
2022-01-27 19:49:39 +0100

    adding styles to Crew component for tablet screens

--- src/components/Member.jsx
+++ src/components/Member.jsx
@@ -6,7 +6,7 @@ const Member = ({ crew }) => {
 				<h3 className='heading3 text-center md:mt-2 md:mb-4'>{crew.name}</h3>
 				<p className='text mx-auto md:mb-10'>{crew.bio}</p>
 			</div>
-			<picture className='centering flex-col h-[223px] -order-1 my-8 md:order-1 md:h-full md:mb-0'>
+			<picture className='centering flex-col h-[223px] -order-1 my-8 md:my-8 md:order-1 md:h-[577px] md:mb-0'>
 				<img src={crew.images.png} alt='Crew member photo' className='h-full' />
 				<hr className='w-full opacity-10 md:hidden ' />
 			</picture>
