0a8bea2ccf47a1fa20d5c26f5e87d0cff5e68b74
Nina <labferment10@gmail.com>
2022-02-02 16:37:43 +0100

    fixing alignment for member's name

--- src/components/Member.jsx
+++ src/components/Member.jsx
@@ -5,7 +5,9 @@ const Member = ({ crew }) => {
 				<h4 className='heading4 opacity-50 text-center lg:text-left'>
 					{crew.role}
 				</h4>
-				<h3 className='heading3 text-center md:mt-2 md:mb-4'>{crew.name}</h3>
+				<h3 className='heading3 text-center md:mt-2 md:mb-4 lg:text-left'>
+					{crew.name}
+				</h3>
 				<p className='text mx-auto md:mb-10'>{crew.bio}</p>
 			</div>
 			<picture className='centering flex-col h-[223px] -order-1 my-8 md:my-8 md:order-1 md:h-[577px] md:mb-0 lg:h-full lg:min-h-[681px] lg:mx-auto justify-self-end'>
